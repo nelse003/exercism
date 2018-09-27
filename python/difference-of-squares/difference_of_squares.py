c990e162e301c36badbc6b89b3aa0fbe2c683598
nelse003 <nelse003@gmail.com>
2018-09-27 22:39:07 +0100

    difference of squares solution

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,49 @@
-def square_of_sum(count):
-    pass
+from numpy import arange
 
+def square_of_sum(count):
+    """Sum of natural numbers up to count, squared
+
+    Parameters
+    ------------
+    count: int
+        Nth nautral number
+
+    Returns
+    ------------
+    int: Sum of natural numbers up to count, squared
+
+    """
+    return arange(0,count+1).sum()**2
 
 def sum_of_squares(count):
-    pass
+    """ Sum of squared natural number up to count.
+
+    Parameters
+    ------------
+    count: int
+        Nth nautral number
+
+    Returns
+    ------------
+    int: Sum of squared natural number up to count
+    """
+    return (arange(0,count+1)**2).sum()
 
 
 def difference(count):
-    pass
+    """ Difference between the square of the sum and the sum of the squares of the first N natural numbers.
+
+    Parameters
+    ------------
+    count: int
+        Nth nautral number
+
+    Returns
+    -----------
+    int:
+        Difference between the square of the sum and
+        the sum of the squares of the first N natural numbers.
+
+    """
+
+    return square_of_sum(count) - sum_of_squares(count)
